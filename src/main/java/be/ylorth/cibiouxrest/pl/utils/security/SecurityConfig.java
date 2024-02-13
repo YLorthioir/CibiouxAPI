94ca74eeb0322d3e8be1daa5416076faa013b0aa
YLorthPro <yann.lorthioir@bstorm.be>
2024-02-13 15:39:28 +0100

    Split separator for cors

--- src/main/java/be/ylorth/cibiouxrest/pl/utils/security/SecurityConfig.java
+++ src/main/java/be/ylorth/cibiouxrest/pl/utils/security/SecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -98,7 +100,7 @@ public class SecurityConfig {
     }
     
     private List<String> allowedOrigins(){
-        return List.of(serverName,serverName.replace("https://", "https://www."));
+        return List.of(serverName.split(", "));
     }
 
 }
